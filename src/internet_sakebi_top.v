b6fc2676f8139fec2660d465146bf940973eb169
Cra2yPierr0t <Cra2yPierr0t@vlsi.jp>
2023-11-09 14:36:33 +0900

    add interface

--- src/internet_sakebi_top.v
+++ src/internet_sakebi_top.v
@@ -1,4 +1,14 @@
 module internet_sakebi_top (
+  input         i_rstn,
 
+  // RMII PHY interface
+  input         i_rmii_txclk,
+  input  [1:0]  i_rmii_rxd,
+  input         i_rmii_crs_dv,
+  output [1:0]  o_rmii_txd,
+  output        o_rmii_txen
 );
+
+  
+
 endmodule
